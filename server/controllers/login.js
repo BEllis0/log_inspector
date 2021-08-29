810e9ff800f0a1c5a4a6196ba351f74b51c46b98
Brandon Ellis <brandonellis.email@gmail.com>
2021-08-29 16:02:14 -0600

    created login controller

---
+++ server/controllers/login.js
@@ -0,0 +1,60 @@
+let User = require('../models/user.model.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+module.exports = {
+    login: (req, res) => {
+        User.find({ email: req.body.email })
+        .then(user => {
+            //check if email address exists
+            if(!user) {
+                console.log('User not found')
+                return res.status(400).json({emailnotfound: "Email not found"});
+            } else { 
+                //compare passwords
+                bcrypt.compare(req.body.password, user[0].password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        
+                        const payload = {
+                            id: user.id,
+                            name: user.username
+                        };
+
+                        jwt.sign(
+                            payload,
+                            process.env.secretOrKey,
+                            {
+                                algorithm: 'HS256',
+                                expiresIn: 31556926 // 1 year in seconds
+                            },
+                            (err, token) => {
+                                console.log(`User ${user[0].username} successfully logged in`);
+                                res.status(200).json({
+                                    success: true,
+                                    message: "Successfully logged in",
+                                    token: token,
+                                });
+                            }
+                        );
+                    }
+                    else {
+                        console.log('Password Incorrect');
+                        return res
+                        .status(400)
+                        .json({ passwordincorrect: "Password incorrect" });
+                    }
+                })
+                .catch(err => {
+                    console.log('Error logging in: ', err);
+                    res.status(400).json("Error" + err)
+                });
+            }
+        })
+        .catch(err => {
+            console.log('Error logging in: ', err);
+            res.status(400).json("Error: " + err)
+        });
+    },
+};
