512e381e01dad0fab7bc97e264914061a5df186c
Brandon Ellis <brandonellis.email@gmail.com>
2021-08-29 13:02:45 -0600

    preparing controller for users

---
+++ server/controllers/users.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+module.exports = {
+    user: {
+        get: (req, res) => {
+
+        },
+        post: (req, res) => {
+
+        },
+        update: (req,  res) => {
+
+        },
+        delete: (req, res) => {
+
+        },
+    },
+};
