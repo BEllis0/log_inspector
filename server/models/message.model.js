d2a35d9380e953302a0375f87c656e4b22d31ac4
Brandon Ellis <brandonellis.email@gmail.com>
2021-08-29 14:21:44 -0600

    first attempt at message schema/model

---
+++ server/models/message.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'); 
+const { Schema } = mongoose;
+const ObjectId = Schema.ObjectId;
+
+const messageSchema = new Schema({
+    siteName: String,
+    environment: String,
+    domain: String, // parsed from the api request. Used for CORs security. Only allow whitelisted domains in the users' domain list
+    page: String,  // either received in req body or parsed from req headers
+    message: {
+        type: Map, // may need to change. current message format: { type: {type: String}, message: Array }
+    },
+    priorityLevel: String,
+    severity: String,
+    status: String,
+    context: String, // exploring how we can dig into the context of the message (outter function, filename, etc)
+    description: String, // entered by users
+    ownerId: ObjectId, // mapped to a user _id
+    companyId: ObjectId, // mapped to a company account
+    projectTicketUrl: String, // optional url to project ticket
+}, {
+    timestamps: true
+});
+
+const Message = mongoose.model('Message', messageSchema);
+module.exports = Message;
