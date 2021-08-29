d23f604a83c664886db913f74282cce4de33e172
Brandon Ellis <brandonellis.email@gmail.com>
2021-08-29 16:00:34 -0600

    added routes

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const whitelist = require('./util/whitelist.js');
 
 // ==== routes
 const messageRouter = require('./routes/messages.js');
-
+const userRouter = require('./routes/users.js');
+const loginRouter = require('./routes/login.js');
 
 // ==== db connection
 // const { connection } = require('./database/connection.js');
@@ -18,13 +20,29 @@ const PORT = process.env.PORT || 8080;
 
 // ==== middleware
 app.use(morgan('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+// cors with domain whitelist generated from user collection
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
+// ==== API handlers
 
 const apiV = '1';
 
 app.use(`/api/v${apiV}/messages`, messageRouter);
+app.use(`/api/v${apiV}/users`, userRouter);
+app.use(`/api/v${apiV}/login`, loginRouter);
 
 // ==== listen
 app.listen(PORT, () => {
