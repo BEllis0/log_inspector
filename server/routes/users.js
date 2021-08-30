c9654db11fc40e6e230cb0ac1eba28a5930cf938
Brandon Ellis <brandonellis.email@gmail.com>
2021-08-29 18:58:40 -0600

    updated put routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,7 +4,10 @@ const usersController = require('../controllers/users.js');
 router.get('/', usersController.user.get.allUsers);
 router.get('/:id', usersController.user.get.byId);
 router.post('/new', usersController.user.post.new);
-router.put('/:id', usersController.user.update.byId);
+router.put('/update/username', usersController.user.update.username);
+router.put('/update/password', usersController.user.update.password);
+router.put('/update/email', usersController.user.update.email);
+router.put('/update/fields', usersController.user.update.fields);
 router.delete('/:id', usersController.user.delete.byId);
 
 module.exports = router;
