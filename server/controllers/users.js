becfea1d473e5214896ae09a789dbd4d785e1655
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-01 15:50:47 -0600

    added error handling and jwt token creation

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,6 @@
-const axios = require('axios');
+const User = require('../models/user.model.js');
+const { generateApiKey } = require('../util/generateApiKey.js');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     user: {
@@ -12,12 +14,60 @@ module.exports = {
         },
         post: {
             new: (req, res) => {
+                const { email, username, password } = req.body;
 
+                if (!(email && password && usernname)) {
+                    res.status(400).json({ message: "Missing information for registration." });
+                }
+
+                //check if email already exists
+                User.findOne({ email: email })
+                .then(user => {
+                    if (user) {
+                      return res.status(400).json({ error: "Email already exists" });
+                    } else {
+                        
+                        const apiKey = generateApiKey().apiKey;
+
+                        const newUser = new User({
+                            email,
+                            username,
+                            password,
+                            clientApiKey: apiKey
+                        });
+
+                        // create JWT token and add to new user
+                        const token = jwt.sign(
+                            { user_id: newUser._id, email },
+                            process.env.JWT_KEY,
+                            {
+                                expiresIn: "2h",
+                            }
+                        );
+
+                        newUser.token = token;
+                
+                        // save user (encryption of pass in pre-save function)
+                        newUser.save()
+                            .then(() => res.status(201).json({ message: `Successfully created new user ${username}` }))
+                            .catch(err => res.status(400).json({ message: "Error saving new user", error: err }));
+                    }
+                })
+                .catch(err => res.status(400).json("Error " + err));
             },
         },
         update: {
-            byId: (req, res) => {
+            username: (req, res) => {
 
+            },
+            password: (req, res) => {
+
+            },
+            email: (req, res) => {
+
+            },
+            fields: (req, res) => {
+                
             },
         },
         delete: {
