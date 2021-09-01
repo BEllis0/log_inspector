9b35ce5b7350add4780ad71292cb551725b175cf
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-01 15:45:14 -0600

    added token authentication to get route

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const messageController = require('../controllers/messages.js');
+const auth = require('../util/auth.js')
 
 router.post('/new', messageController.messages.post.new);
+router.get('/all', auth, messageController.messages.get.byUser);
 
 module.exports = router;
