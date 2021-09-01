d700c59633787291ff7fd51408a5019575a9c312
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-01 15:49:54 -0600

    finalized new message format

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,19 +1,46 @@
-const axios = require('axios');
+const Message = require('../models/message.model.js');
+const { messageValidation } = require('../util/messageValidation.js');
 
 module.exports = {
     messages: {
         post: {
             new: (req, res) => {
-                // add message to db
-                console.log('message received', req.body)
-                console.log('host', req.hostname)
-                res.json("got your message");
+                messageValidation(req.get('apiKey'), req.hostname)
+                    .then(validatedPerson => {
+                        const messagePayload = {
+                            siteName: req.body.siteName || null,
+                            environment: req.body.environment || null,
+                            domain: req.hostname,
+                            ownerId: validatedPerson.personId || undefined, // returned from validation process
+                            page: req.protocol + "://" + req.hostname + req.originalUrl || null,
+                            message: req.body.message || null,
+                            priorityLevel: req.body.priorityLevel || null,
+                            severity: req.body.severity || null,
+                            status: req.body.status || null,
+                            context: req.body.context || null,
+                            description: req.body.description || null,
+                            projectTicketUrl: req.body.projectTicketUrl || null
+                        };
+
+                        const newMessage = Message(messagePayload);
+
+                        newMessage.save()
+                            .then(() => res.status(200).json({ message: "Message saved" }))
+                            .catch(err => {
+                                console.log("Error saving message", err);
+                                res.status(500).json({ message: "Error saving message", error: err });
+                            });
+                    })
+                    .catch(err => {
+                        return res.status(403).json(err);
+                    });
             },
         },
         get: {
-            all: (req, res) => {
+            byUser: (req, res) => {
                 // get messages from db
             },
+            
         },
     },
 };
