cff0d8c41b41de70cd8ded20c0beafedf65c7055
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-01 15:47:40 -0600

    updated connection params

--- database/config.js
+++ database/config.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // set the connection options, which will be applied to all connections
+// depreciated after mongoose v5.13.8
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-const uri = process.env.ATLAS_URI;
-
-const connection = mongoose.connect(uri)
+const connection = mongoose.connect(process.env.ATLAS_URI)
     .then(() => console.log("MongoDB database connection established successfully"))
     .catch(err => console.log(err));
 
