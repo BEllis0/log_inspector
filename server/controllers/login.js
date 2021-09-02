84a2180e9f7db2eab041099c35a1c7ac6c53abdf
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-02 17:31:00 -0600

    adding req.cookie with refresh token

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -5,7 +5,14 @@ require('dotenv').config();
 
 module.exports = {
     login: (req, res) => {
-        User.find({ email: req.body.email })
+
+        const { email, username, password } = req.body;
+
+        if (!(email && password)) {
+            res.status(400).send({ message: "Missing email or password for login." });
+        }
+
+        User.find({ email })
         .then(user => {
             //check if email address exists
             if(!user) {
@@ -14,51 +21,48 @@ module.exports = {
             }
 
             //compare passwords
-            bcrypt.compare(req.body.password, user[0].password)
+            bcrypt.compare(password, user[0].password)
             .then(isMatch => {
                 if(isMatch) {
                     
                     const payload = {
-                        id: user.id,
-                        name: user.username
+                        user_id: user[0]._id,
+                        username: user[0].username,
+                        email: user[0].email,
+                        whitelist: user[0].domains,
+                        company: user[0].company,
                     };
 
-                    jwt.sign(
+                    const token = jwt.sign(
                         payload,
                         process.env.JWT_KEY,
                         {
-                            algorithm: 'HS256',
-                            expiresIn: 31556926 // 1 year in seconds
+                            expiresIn: "15m" // 1 year in seconds
                         },
-                        (err, token) => {
-                            if (err) {
-                                console.log('Error generating JWT in login.')
-                                res.status(500).json({message: "Error logging in."});
-                            }
-                            console.log(`User ${user[0].username} successfully logged in`);
-                            res.status(200).json({
-                                success: true,
-                                message: "Successfully logged in",
-                                token: token,
-                            });
-                        }
                     );
-                }
-                else {
-                    console.log('Password Incorrect');
-                    return res
-                    .status(400)
-                    .json({ message: "Password incorrect" });
+
+                    // attach refresh token
+                    res.cookie("refreshToken", user[0]._id, {
+                        secure: process.env.NODE_ENV !== "development",
+                        httpOnly: true,
+                        expires: new Date(Date.now() + (60000 * 24)),
+                    });
+                        
+                    res.status(200).json({
+                        success: true,
+                        message: "Successfully logged in",
+                        token: token,
+                    });
                 }
             })
             .catch(err => {
                 console.log('Error logging in: ', err);
-                res.status(400).json("Error" + err)
+                res.status(400).json({message: "Incorrect password."});
             });
         })
         .catch(err => {
             console.log('Error logging in: ', err);
-            res.status(400).json("Error: " + err)
+            res.status(400).json({message: "Email not found."});
         });
     },
 };
