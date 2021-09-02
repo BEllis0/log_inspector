22548594f7ba04711a7b82defa6383b8c7cf7576
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-02 17:27:08 -0600

    removed logs

--- server/util/auth.js
+++ server/util/auth.js
@@ -1,19 +1,21 @@
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 const verifyToken = (req, res, next) => {
-  const token =
-    req.body.token || req.query.token || req.headers["x-access-token"];
+    const token = req.body.token || req.query.token || req.headers["x-access-token"];
 
-  if (!token) {
-    return res.status(403).send({message: "A token is required for authentication"});
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-    req.user = decoded;
-  } catch (err) {
-    return res.status(401).send({message: "Invalid Token"});
-  }
-  return next();
+    if (!token) {
+        return res.status(403).send({message: "A token is required for authentication"});
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_KEY);
+        req.user = decoded;
+    } catch (err) {
+        return res.status(401).send({message: "Invalid Token", err: err });
+    }
+
+    return next();
 };
 
 module.exports = verifyToken;
