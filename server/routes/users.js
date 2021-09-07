477d4db0647917f0ffa1943045f8b0e9fc094148
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-06 19:45:05 -0600

    changed from put to patch

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,10 +4,10 @@ const auth = require('../util/auth.js');
 
 router.get('/', auth, usersController.user.get.byId);
 router.post('/new', usersController.user.post.new);
-router.put('/update/username', auth, usersController.user.update.username);
-router.put('/update/password', auth, usersController.user.update.password);
-router.put('/update/email', auth, usersController.user.update.email);
-router.put('/update/fields', auth, usersController.user.update.fields);
+router.patch('/update/username', auth, usersController.user.update.username);
+router.patch('/update/password', auth, usersController.user.update.password);
+router.patch('/update/email', auth, usersController.user.update.email);
+router.patch('/update/fields', auth, usersController.user.update.fields);
 router.delete('/delete', auth, usersController.user.delete.byId);
 
 module.exports = router;
