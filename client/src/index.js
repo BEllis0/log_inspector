c01f7ba282a61efcfa2249c0c84cb4cbe973d8ba
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-15 19:39:26 -0600

    init client

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import { Provider } from 'react-redux';
+//import store from './store.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App.jsx';
+
+ReactDOM.render((
+    // <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+    // </Provider>
+    ), document.getElementById('app'));
