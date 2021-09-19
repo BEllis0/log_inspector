3dcbf68b1a01ad926789419642ca841995f248e9
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-18 21:45:32 -0600

    loading global scss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { withRouter, BrowserHistory } from 'react-router';
 import { Route } from "react-router-dom";
 // redux
 import { connect } from 'react-redux'; // connect to store
+import styles from './global.scss';
 
 // components
 import Home from './components/Views/Home/Home.jsx';
