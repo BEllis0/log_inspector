f5f6bcf52199d5648d6d50897d7de948c4330961
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-18 21:38:05 -0600

    resolved all issues with scss rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,82 @@
-const isDev = process.env.NODE_ENV === 'development';
 var path = require('path');
 var SRC_DIR = path.join(__dirname, '/client/src');
 var DIST_DIR = path.join(__dirname, '/client/public');
-module.exports = {
-    mode: isDev ? 'development' : 'production',
-    entry: [
-        '@babel/polyfill', // enables async-await
-        `${SRC_DIR}/index.js`
-    ],
-    output: {
-        path: DIST_DIR,
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.scss']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, options) => {
+    const isDev = options.mode !== 'production';
+    return {
+        mode: isDev ? 'development' : 'production',
+        entry: [
+            '@babel/polyfill', // enables async-await
+            `${SRC_DIR}/index.js`
+        ],
+        output: {
+            path: DIST_DIR,
+            filename: 'bundle.js'
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.scss']
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: isDev ? '[name].css' : '[name].[hash].css',
+                chunkFilename: isDev ? '[id].css' : '[id].[hash].css'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
                     }
-                }
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'resolve-url-loader'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
                 },
-            }
-        ]
+                {
+                    test: /\.module\.s(a|c)ss$/,
+                    use: [
+                        { loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                sourceMap: isDev
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDev
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.s(a|c)ss$/,
+                    exclude: /\.module\.(s(a|c)ss)$/,
+                    use: [
+                        { loader: isDev ? 'style-loader' : MiniCssExtractPlugin.loader },
+                        { loader: 'css-loader' },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDev
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                    },
+                }
+            ]
+        }
     }
 };
