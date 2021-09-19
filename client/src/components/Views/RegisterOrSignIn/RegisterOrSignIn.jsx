fd3489bd59916067059d41e60150857dfec0fe80
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-18 21:49:21 -0600

    conditional display of sign-in or register form

--- client/src/components/Views/RegisterOrSignIn/RegisterOrSignIn.jsx
+++ client/src/components/Views/RegisterOrSignIn/RegisterOrSignIn.jsx
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router';
 // redux
 import { connect } from 'react-redux'; // connect to store
 
+import PrimaryNav from '../../Nav/Primary/PrimaryNav.jsx';
 import RegisterForm from '../../Forms/Register/RegisterForm.jsx';
 import SignInForm from '../../Forms/SignIn/SignInForm.jsx';
 
@@ -14,39 +15,41 @@ const theme = createTheme();
 
 const RegisterOrSignIn = (props) => {
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
-          sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
+    return (
+        <ThemeProvider theme={theme}>
+
+            <PrimaryNav />
+
+            <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    backgroundImage: 'url(https://source.unsplash.com/random)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                    t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+                />
 
 
-        {/* Determine which form to display */}
+                {/* Determine which form to display */}
 
-        {props.location.pathname === "/register" &&
-            <RegisterForm />
-        }
-        
-        {props.location.pathname === "/sign-in" &&
-            <SignInForm />
-        }
+                {props.location.pathname === "/register" &&
+                    <RegisterForm />
+                }
+                
+                {props.location.pathname === "/sign-in" &&
+                    <SignInForm />
+                }
       
-      
-      </Grid>
-    </ThemeProvider>
+            </Grid>
+        </ThemeProvider>
   );
 }
 
