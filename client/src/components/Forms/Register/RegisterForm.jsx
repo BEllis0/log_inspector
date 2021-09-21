d1bf1661f176a2ac1a6747373fef3ed96d5acf4d
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-21 12:24:26 -0600

    fixed redux issue, added dispatch to props

--- client/src/components/Forms/Register/RegisterForm.jsx
+++ client/src/components/Forms/Register/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { withRouter } from 'react-router';
 import Copyright from '../../Misc/Copyright.jsx';
+import { register } from '../../../actions/register.js';
 
 // redux
 import { connect } from 'react-redux'; // connect to store
@@ -20,98 +21,131 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 const theme = createTheme();
 
 const RegisterForm = (props) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="fname"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="lname"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/sign-in" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
-  );
-}
+
+    const [firstName, updateFirstName] = React.useState('');
+    const [lastName, updateLastName] = React.useState('');
+    const [email, updateEmail] = React.useState('');
+    const [password, updatePassword] = React.useState('');
+    const [username, updateUsername] = React.useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.register({
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            email: email,
+            password: password
+        });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+                >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <Box onSubmit={handleSubmit} component="form" noValidate sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                        onChange={e => updateFirstName(e.target.value)}
+                        autoComplete="fname"
+                        name="firstName"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                        onChange={e => updateLastName(e.target.value)}
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="lname"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        onChange={e => updateUsername(e.target.value)}
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete="username"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        onChange={e => updateEmail(e.target.value)}
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        onChange={e => updatePassword(e.target.value)}
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        />
+                    </Grid>
+                    </Grid>
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    >
+                    Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                    <Grid item>
+                        <Link href="/sign-in" variant="body2">
+                        Already have an account? Sign in
+                        </Link>
+                    </Grid>
+                    </Grid>
+                </Box>
+                </Box>
+                <Copyright sx={{ mt: 5 }} />
+            </Container>
+        </ThemeProvider>
+    );
+};
 
 const mapStateToProps = state => ({
     state: state
 });
 
 const mapDispatchToProps = {
-    // loginCheck,
+    register
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(RegisterForm));
--- client/src/components/Forms/SignIn/SignInForm.jsx
+++ client/src/components/Forms/SignIn/SignInForm.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { withRouter } from 'react-router';
+import { login } from '../../../actions/login.js';
 
 import Copyright from '../../Misc/Copyright.jsx';
 
@@ -10,85 +11,102 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 
-const SignInForm = () => {
+const SignInForm = props => {
 
-  return (
-    <Grid item xs={12} sm={8} md={5} elevation={6} square>
-        <Box
-        sx={{
-            my: 8,
-            mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        }}
-        >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-            Sign in
-        </Typography>
-        <Box component="form" noValidate sx={{ mt: 1 }}>
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            />
-            <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            />
-            <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.login({
+            email: email,
+            password: password
+        });
+    };
+
+    return (
+        <Grid item xs={12} sm={8} md={5} elevation={6} square="true">
+            <Box
+            sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
             >
-            Sign In
-            </Button>
-            <Grid container>
-            <Grid item xs>
-                <Link href="#" variant="body2">
-                Forgot password?
-                </Link>
-            </Grid>
-            <Grid item>
-                <Link href="/register" variant="body2">
-                {"Don't have an account? Sign Up"}
-                </Link>
-            </Grid>
-            </Grid>
-            <Copyright sx={{ mt: 5 }} />
-        </Box>
-        </Box>
-    </Grid>
-  );
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+            <Box
+                onSubmit={handleSubmit}
+                component="form" 
+                noValidate 
+                sx={{ mt: 1 }}
+            >
+                <TextField
+                onChange={e => setEmail(e.target.value)}
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                />
+                <TextField
+                onChange={e => setPassword(e.target.value)}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                />
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                >
+                Sign In
+                </Button>
+                <Grid container>
+                <Grid item xs>
+                    <Link href="#" variant="body2">
+                    Forgot password?
+                    </Link>
+                </Grid>
+                <Grid item>
+                    <Link href="/register" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                    </Link>
+                </Grid>
+                </Grid>
+                <Copyright sx={{ mt: 5 }} />
+            </Box>
+            </Box>
+        </Grid>
+    );
 }
 
 const mapStateToProps = state => ({
-    state: state
+    user: state
 });
 
 const mapDispatchToProps = {
-    // loginCheck,
+    login,
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SignInForm));
