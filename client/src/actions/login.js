d5503773a32e57bd75a8470fcad2cd7ae8f92a5a
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-22 15:36:48 -0600

    returning promise

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -1,21 +1,17 @@
 import { actions } from './types.js';
 import Axios from 'axios';
+import { AuthPost } from '../util/auth.js';
 
 export function login(payloadData) {
-    console.log('payload data: ', payloadData)
     return function(dispatch) {
-        Axios.post('http://localhost:8080/api/v1/login/', {
-            headers: {
-                "content-type": "application/json",
-            },
-            data: payloadData
-        })
+        return AuthPost('/api/v1/login/', payloadData)
         .then(response => {
-            console.log('login response: ', response)
             dispatch({
                 type: actions.login.LOGIN,
                 payload: response.data
             });
+
+            return Promise.resolve();
         })
         .catch(error => {
             console.log('login error: ', error)
