4972edd8d63a18d8beba3a47e0216672bc9d605a
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-22 22:18:15 -0600

    building profile view with user details, whitelist, api key etc

--- client/src/components/Views/Profile/Profile.jsx
+++ client/src/components/Views/Profile/Profile.jsx
@@ -4,19 +4,97 @@ import { Redirect } from "react-router-dom";
 import { connect } from 'react-redux'; // connect to store
 import styles from './Profile.module.scss';
 
+import AuthNav from '../../Nav/AuthNav/AuthNav.jsx';
+import PrimaryNav from '../../Nav/Primary/PrimaryNav.jsx'
+import ProfileTabs from '../../Nav/ProfileTabs/ProfileTabs.jsx';
+import NewDomainForm from '../../Forms/NewDomain/NewDomain.jsx';
+
 const ProfileView = (props) => {
+
+    const { user, location, loggedIn } = props;
+
+    if (loggedIn === false) {
+        return (
+            <Redirect to="/sign-in" />
+        )
+    }
     
     return (
-        <>
-        {!props.loggedIn &&
-            <Redirect to="/sign-in" />
-        }
+        <div>
+        
+        <PrimaryNav />
+        <AuthNav />
+        
+        <div className="container">
 
-        {props.loggedIn &&
-            <div>User logged in</div>
+        <ProfileTabs />
+
+        {location.pathname === "/account/profile" &&
+            <div className="widgetContainer">
+                <div className="widget flex">
+                    <div className="w6">
+                        <h3 className="subHeading">Account Details</h3>
+                        <br />
+                        <div>
+                            <p>Name</p>
+                            <p>{user.firstName} {user.lastName}</p>
+                        </div>
+                        <br />
+                        <div>
+                            <p>Email Address</p>
+                            <p>{user.email}</p>
+                        </div>
+                        <br />
+                        <div>
+                            <p>Username</p>
+                            <p>{user.username}</p>
+                        </div>
+                    </div>
+                    <div className="w6">
+                        <h3 className="subHeading">Client API Key</h3>
+                        <br />
+                        <p>API Key Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum
+                        </p>
+                        <br />
+                        <p className="orangeBorderBox">{user.clientApiKey}</p>
+                    </div>
+                </div>
+
+                <div className="widget">
+                    <h3 className="subHeading">Domain Whitelist</h3>
+                    <br />
+                    <p>Domain whitelist Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit.
+                    </p>
+                    <br />
+                    <div>
+                        <div className="orangeBorderBox flex">
+                            <p className="secondaryCTA">Add a Domain +</p>
+                            <NewDomainForm />
+                        </div>
+                        <ul className="flex">
+                        {user.domains && user.domains.map(domain => {
+                            return (
+                                <li key={domain} className={styles.domain}>{domain}</li>
+                            )
+                        }) 
+
+                        }
+                        </ul>
+                    </div>
+                </div>
+            </div>
         }
         
-        </>
+        </div>
+        
+        </div>
     )
     
 };
@@ -27,7 +105,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-    // loginCheck,
+
 };
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProfileView));
