e68b0d9f4515cc9e4d5539b2e615db1f54dd893f
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-24 20:37:34 -0600

    only saving login boolean to local storage

--- client/src/store.js
+++ client/src/store.js
@@ -8,22 +8,36 @@ import throttle from 'lodash.throttle';
 
 const middleware = [thunk];
 
+/*
+Only saving login status on local storage
+Avoiding saving sensitive user info on storage
+Login status is referenced in App comp to get data on mount
+returning login property to mimic the expected state data format
+*/
+
 const loadState = () => {
   try {
     const serializedState = localStorage.getItem('state');
     if(serializedState === null) {
       return undefined;
     }
-    return JSON.parse(serializedState);
+    return {login: JSON.parse(serializedState)}
+    // return JSON.parse(serializedState);
   } catch (e) {
       console.log("Error loading state from local storage", e);
     return undefined;
   }
 };
-  
+
+/*
+Only saving login status on local storage
+Avoiding saving sensitive user info on storage
+Login status is referenced in App comp to get data on mount
+*/
+
 const saveState = (state) => {
   try {
-    const serializedState = JSON.stringify(state);
+    const serializedState = JSON.stringify(state.login);
     localStorage.setItem('state', serializedState);
   } catch (e) {
     console.log('Error saving state to local storage', e)
@@ -57,8 +71,8 @@ if (window.__REDUX_DEVTOOLS_EXTENSION__ !== undefined) {
   );
 }
 
-// store.subscribe(throttle(() => {
-//     saveState(store.getState());
-//   }, 1000));
+store.subscribe(throttle(() => {
+    saveState(store.getState());
+  }, 1000));
 
 export default store;
