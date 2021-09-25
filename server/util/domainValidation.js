edd54137f3fb47aa49a65bf21c97afb91bf4257a
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-24 20:35:53 -0600

    returns boolean of whether whitelist domain is valid

---
+++ server/util/domainValidation.js
@@ -0,0 +1,3 @@
+module.exports.domainValidation = (domainName) => {
+    return /^(((?!-))(xn--|_{1,1})?[a-z0-9-]{0,61}[a-z0-9]{1,1}\.)*(xn--)?([a-z0-9][a-z0-9\-]{0,60}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.test(domainName);
+}
