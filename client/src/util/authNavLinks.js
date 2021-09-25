3bcba163d20d372d7ff30e2f89eb70f777982bfa
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-24 20:39:35 -0600

    created array of nav items for auth nav

---
+++ client/src/util/authNavLinks.js
@@ -0,0 +1,6 @@
+export default [
+    { name: "Dashboard", href: "/dashboard" },
+    { name: "Projects", href: "/projects" },
+    { name: "Archive", href: "/archive" },
+    { name: "Profile", href: "/account/profile" },
+];
