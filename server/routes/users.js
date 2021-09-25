6613ff2bfbb88e6fc091454715e7ca0206e1ae55
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-24 20:36:22 -0600

    added route for deleting domain

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,7 +7,8 @@ router.post('/new', usersController.user.post.new);
 router.patch('/update/username', auth, usersController.user.update.username);
 router.patch('/update/password', auth, usersController.user.update.password);
 router.patch('/update/email', auth, usersController.user.update.email);
-router.patch('/update/fields', auth, usersController.user.update.fields);
+router.patch('/update/domains', auth, usersController.user.update.domains);
+router.delete('/delete/domain', auth, usersController.user.delete.domain);
 router.delete('/delete', auth, usersController.user.delete.byId);
 
 module.exports = router;
