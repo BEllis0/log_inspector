6479a8181c7e65739f0db4fa4242da2bdac24b83
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-29 15:32:48 -0600

    added GET route for server polling

--- server/routes/refresh.js
+++ server/routes/refresh.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const refreshController = require('../controllers/refresh.js');
 const auth = require('../util/auth.js')
 
+router.get('/', refreshController.get.poll);
 router.post('/', refreshController.post.refresh);
 
 module.exports = router;
