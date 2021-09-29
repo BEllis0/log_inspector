3f8267424901be8c9d59b8c0030a726be6af536f
Brandon Ellis <brandonellis.email@gmail.com>
2021-09-29 14:43:21 -0600

    finished logout route and handler'

--- server/controllers/logout.js
+++ server/controllers/logout.js
@@ -1,10 +1,9 @@
 module.exports = {
     logout: (req, res) => {
         const { user_id } = req.user;
-
         if (user_id) {
             res.clearCookie('token');
-            res.status(200).redirect('/');
+            res.status(200).json({message: "Successfully logged out."});
         } else {
             res.status(400).json({ message: "Cannot logout, user is not logged in." });
         }
--- server/routes/logout.js
+++ server/routes/logout.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const logoutController = require('../controllers/logout.js');
+const auth = require('../util/auth.js');
 
-router.post('/', logoutController.logout);
+router.post('/', auth, logoutController.logout);
 
 module.exports = router;
