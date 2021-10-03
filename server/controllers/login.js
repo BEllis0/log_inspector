9a465b7813d5d3c83eb412a587b0d278ed5f2d5c
Brandon Ellis <brandonellis.email@gmail.com>
2021-10-03 15:06:26 -0600

    restructured login promise to async await

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -4,67 +4,71 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = {
-    login: (req, res) => {
+    login: async (req, res) => {
 
-        const { email, username, password } = req.body.data;
+        const { email, username, password } = req.body.data || req.body;
 
-        if (!(email && password)) {
-            res.status(400).send({ message: "Missing email or password for login." });
+        // missing email
+        if (!email === true) {
+            return res.status(400).json({ message: "Missing email for login." });
         }
 
-        User.find({ email })
-        .then(user => {
-            //check if email address exists
-            if(!user) {
-                console.log('Login: User not found')
-                return res.status(400).json({ message: "Email not found" });
-            }
+        // missing password
+        if (!password === true) {
+            return res.status(400).json({ message: "Missing password for login." });
+        }
 
-            //compare passwords
-            bcrypt.compare(password, user[0].password)
-            .then(isMatch => {
-                if(isMatch) {
+        const user = await User.findOne({ email: email });
+
+        // user not found
+        if (!user) {
+            console.log('User email not found.')
+            return res.status(400).json({ message: "Email not found." });
+        }
+
+        //compare passwords
+        bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if(isMatch) {
+                
+                const payload = {
+                    user_id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    whitelist: user.domains,
+                    company: user.company,
+                };
+
+                const token = jwt.sign(
+                    payload,
+                    process.env.JWT_KEY,
+                    {
+                        expiresIn: "15m"
+                    },
+                );
+
+                // attach refresh token
+                res.cookie("token", token, {
+                    secure: process.env.NODE_ENV !== "development",
+                    httpOnly: true,
+                    sameSite: true,
+                    expires: new Date(Date.now() + (60000 * 24)),
+                });
                     
-                    const payload = {
-                        user_id: user[0]._id,
-                        username: user[0].username,
-                        email: user[0].email,
-                        whitelist: user[0].domains,
-                        company: user[0].company,
-                    };
-
-                    const token = jwt.sign(
-                        payload,
-                        process.env.JWT_KEY,
-                        {
-                            expiresIn: "15m"
-                        },
-                    );
-
-                    // attach refresh token
-                    res.cookie("token", token, {
-                        secure: process.env.NODE_ENV !== "development",
-                        httpOnly: true,
-                        sameSite: true,
-                        expires: new Date(Date.now() + (60000 * 24)),
-                    });
-                        
-                    res.status(200).json({
-                        success: true,
-                        message: "Successfully logged in",
-                        token: token,
-                        user_id: user[0].id
-                    });
-                }
-            })
-            .catch(err => {
-                console.log('Error logging in: ', err);
-                res.status(400).json({message: "Incorrect password."});
-            });
+                return res.status(200).json({
+                    success: true,
+                    message: "Successfully logged in",
+                    // token: token,
+                    user_id: user.id
+                });
+            } else {
+                return res.status(400).json({message: "Incorrect password."});
+            }
         })
         .catch(err => {
-            console.log('Error logging in: ', err);
-            res.status(400).json({message: "Email not found."});
+            console.log('Error comparing passwords: ', err);
+            res.status(400).json({message: "Incorrect password."});
         });
+
     },
 };
