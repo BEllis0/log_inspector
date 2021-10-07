cd6539a5d4b0a9281b3063021512660f4927b7af
Brandon Ellis <brandonellis.email@gmail.com>
2021-10-07 16:02:59 -0600

    removed margins

--- client/src/components/Nav/PublicFooter/PublicFooter.jsx
+++ client/src/components/Nav/PublicFooter/PublicFooter.jsx
@@ -9,20 +9,20 @@ const PublicFooter = props => {
         <div className={styles.publicFooterContainer}>
             <div className="container">
                 <div className="flex gap-30">
-                    <div className="fb4 margin-lr-50">
+                    <div className="fb4">
                         <h3 className="footerHeader">Navigation</h3>
                         <p className="footerLink"><Link to="/">Home</Link></p>
                         <p className="footerLink"><Link to="/sign-in">Sign In</Link></p>
                         <p className="footerLink"><Link to="/register">Register</Link></p>
                     </div>
-                    <div className="fb4 margin-lr-50">
+                    <div className="fb4">
                         <h3 className="footerHeader">log-inspector</h3>
                         <p className="footerLink"><Link to="/documentation">Documentation</Link></p>
                         <p className="footerLink"><a href="https://www.npmjs.com/package/log-inspector" target="_blank">NPM Docs</a></p>
                         <p className="footerLink"><a href="https://yarnpkg.com/package/log-inspector" target="_blank">Yarn Docs</a></p>
                         <p className="footerLink"><a href="https://github.com/BEllis0/_logger" target="_blank">Github</a></p>
                     </div>
-                    <div className="fb4 margin-lr-50">
+                    <div className="fb4">
                         <h3 className="footerHeader">Open Source</h3>
                         <p className="footerLink"><a href="https://github.com/BEllis0/log_inspector" target="_blank">Github</a></p>
                     </div>
