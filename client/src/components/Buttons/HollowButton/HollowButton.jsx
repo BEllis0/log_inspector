d263daf920fa6e341e4edfaa1d41fd6ca95b005d
Brandon Ellis <brandonellis.email@gmail.com>
2021-10-14 12:54:29 -0600

    added actions for api href links, using handleclick instead of Link

--- client/src/components/Buttons/HollowButton/HollowButton.jsx
+++ client/src/components/Buttons/HollowButton/HollowButton.jsx
@@ -2,28 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './HollowButton.module.scss';
 import { Link } from 'react-router-dom';
+import { withRouter, useLocation } from 'react-router';
+
+import { connect } from 'react-redux'; // connect to store
+
+import { getMessagesByUser, updatedMessages } from '../../../actions/messages.js';
 
 const HollowButton = (props) => {
-    const { text, color, href } = props;
+    const { 
+        text,
+        color,
+        href,
+        api,
+        history,
+        getMessagesByUser,
+        updatedMessages
+    } = props;
 
     const style = {
         color: color ? color : '',
         borderColor: color ? color : ''
     };
 
+    const handleClick = async (e) => {
+        e.preventDefault();
+        if (api) {
+            if (/messages/ig.test(href)) {
+                await getMessagesByUser();
+                await updatedMessages();
+            }
+        } else {
+            history.push(href);
+        }
+    }
+
     return (
-        <Link to={href}>
             <button
                 className={styles.hollowButton}
                 style={style}
+                onClick={handleClick}
             >
                 { text }
             </button>
-        </Link>
     )
 };
 
-export default HollowButton;
+const mapDispatchToProps = {
+    getMessagesByUser,
+    updatedMessages
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(HollowButton));
 
 HollowButton.propTypes = {
     text: PropTypes.string,
