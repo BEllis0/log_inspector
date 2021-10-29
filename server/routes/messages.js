33475027ff6318207dd6a737c8c1ec9dc34372ac
Brandon Ellis <brandonellis.email@gmail.com>
2021-10-29 01:11:39 -0600

    added route for removing messages by id

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -4,5 +4,6 @@ const auth = require('../util/auth.js')
 
 router.post('/new', messageController.messages.post.new);
 router.get('/all', auth, messageController.messages.get.byUser);
+router.delete('/remove', auth, messageController.messages.delete.byIds);
 
 module.exports = router;
